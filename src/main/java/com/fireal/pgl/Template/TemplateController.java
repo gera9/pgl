7bda76ad472d235a5eed5b712169605d3b6279e4
Gerado M. R. <mmorales@tredicom.com>
2025-06-30 10:31:18 -0600

    sort imports

--- src/main/java/com/fireal/pgl/Template/TemplateController.java
+++ src/main/java/com/fireal/pgl/Template/TemplateController.java
@@ -5,13 +5,13 @@ import java.util.UUID;
 import java.util.stream.Collectors;
 
 import org.springframework.http.HttpStatus;
-import org.springframework.http.HttpStatusCode;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -20,7 +20,6 @@ import org.springframework.web.bind.annotation.RestController;
 
 import com.fireal.pgl.Template.DTOs.TemplateDTO;
 import com.fireal.pgl.Template.DTOs.TemplateMapper;
-import org.springframework.web.bind.annotation.PutMapping;
 
 @RestController
 @RequestMapping(path = "/templates")
